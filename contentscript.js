459d4688899c9be956ad21dec1eb22718675117a
Yury Molchanov <yamolchanov@yandex-team.ru>
2020-05-11 09:37:16 +0300

    1-send-to-console-concatenated-zoneNames

--- contentscript.js
+++ contentscript.js
@@ -1,3 +1,25 @@
+function traverseParents(el, cb) {
+  cb(el);
+
+  if (el.nodeName === "BODY") {
+    return;
+  }
+
+  return traverseParents(el.parentElement, cb);
+}
+
+const findZones = (acc) => (el) => {
+  const { zoneName } = el.dataset;
+
+  if (zoneName) {
+    acc.push(zoneName);
+  }
+};
+
 document.body.addEventListener("mouseover", (event) => {
-  console.log(event);
+  const zoneNames = [];
+
+  traverseParents(event.target, findZones(zoneNames));
+
+  console.log(zoneNames.reverse().join("_"));
 });
